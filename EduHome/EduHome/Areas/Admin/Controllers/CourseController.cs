65382c863d465c5b4bd89a109776997d466a33be
Parviz Asad <parvizya@code.edu.az>
2022-03-09 01:46:52 +0400

    Course Delete finished

--- EduHome/EduHome/Areas/Admin/Controllers/CourseController.cs
+++ EduHome/EduHome/Areas/Admin/Controllers/CourseController.cs
@@ -31,7 +31,7 @@ namespace EduHome.Areas.Admin.Controllers
             int take = 10;
             ViewBag.totalpage = Math.Ceiling((decimal)_dbContext.Courses.Count() / take);
             ViewBag.currentpage = page;
-            var courses = await _dbContext.Courses.Skip((page - 1) * take).Take(take).ToListAsync();
+            var courses = await _dbContext.Courses.Where(x=> x.IsDeleted==false).Skip((page - 1) * take).Take(take).ToListAsync();
             return View(courses);
         }
 
@@ -258,191 +258,39 @@ namespace EduHome.Areas.Admin.Controllers
             return RedirectToAction("Index");
         }
 
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
 
+            var course = await _dbContext.Courses
+                .Where(x => x.Id == id && x.IsDeleted == false)
+                .FirstOrDefaultAsync();
+            if (course == null)
+                return NotFound();
 
-        //public async Task<IActionResult> Update(int? id)
-        //{
-        //    var categories = await _dbContext.Categories.Where(x => x.IsDeleted == false).ToListAsync();
-        //    ViewBag.Categories = categories;
+            return View(course);
+        }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeleteCategory(int? id)
+        {
+            if (id == null)
+                return NotFound();
 
+            var course = await _dbContext.Courses.FindAsync(id);
+            if (course == null)
+                return NotFound();
 
-        //    if (id == null)
-        //        return NotFound();
+            course.IsDeleted = true;
+            await _dbContext.SaveChangesAsync();
 
-        //    var course = await _dbContext.Courses.FirstOrDefaultAsync(x => x.Id == id);
+            return RedirectToAction(nameof(Index));
 
-        //    if (course == null)
-        //        return NotFound();
+        }
 
-        //    var selectedParentCategoryId = course.CourseCategories.Where(x => x.CourseID == id && x.Categories.IsDeleted == false);
-
-
-        //    var parentCategories = await _dbContext.Categories
-        //     .Where(x => x.IsDeleted == false )
-        //     .ToListAsync();
-
-        //    var courseViewModel = new CourseViewModel
-        //    {
-        //        Id = course.Id,
-        //        Name = course.Name,
-        //        Description = course.Description,
-        //    Assestments=course.CourseDetail.Assestments,
-        //    AboutDescription=course.CourseDetail.AboutDescription,
-        //    ApplyDescription=course.CourseDetail.ApplyDescription,
-        //    CertificaitonDescription=course.CourseDetail.CertificaitonDescription,
-        //    DurationTime=course.CourseDetail.DurationTime,
-        //    ImagePath=course.ImagePath,
-        //    Language=course.CourseDetail.Language,
-        //    LessonDurationTime=course.CourseDetail.LessonDurationTime,
-        //    ParentCategories=parentCategories,
-        //    SkillLevel=course.CourseDetail.SkillLevel,
-        //    Price=course.CourseDetail.Price,
-        //    StartDay=course.CourseDetail.StartDay,  
-        //    studentCapacity=course.CourseDetail.studentCapacity,
-
-        //    };
-
-        //    return View(courseViewModel);
-        //}
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Update(int? id, CourseViewModel courseViewModel)
-        //{
-        //    var categories = await _dbContext.Categories.Where(x => x.IsDeleted == false).ToListAsync();
-        //    ViewBag.Categories = categories;
-
-
-        //    if (id == null)
-        //        return NotFound();
-
-        //    if (id != courseViewModel.Id)
-        //        return BadRequest();
-
-        //    if (!ModelState.IsValid)
-        //        return View();
-
-        //    var existCourse = await _dbContext.Courses.FindAsync(id);
-        //    if (existCourse == null)
-        //        return NotFound();
-
-        //    var isExist = await _dbContext.Courses
-        //        .AnyAsync(x => x.Name.ToLower().Trim() == courseViewModel.Name.ToLower().Trim() && x.Id != id);
-        //    if (isExist)
-        //    {
-        //        ModelState.AddModelError("Name", "Eyni adda course movcuddur");
-        //        return View();
-        //    }
-
-        //    existCourse.Name = courseViewModel.Name;
-
-        //    await _dbContext.SaveChangesAsync();
-
-        //    return RedirectToAction(nameof(Index));
-        //}
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Update(int? id, ProductViewModel productViewModel)
-        //{
-        //    if (id == null)
-        //        return NotFound();
-
-        //    if (id != productViewModel.Id)
-        //        return BadRequest();
-
-        //    var existProduct = await _dbContext.Products
-        //        .Where(x => x.Id == id)
-        //        .Include(x => x.ProductImages)
-        //        .Include(x => x.ProductCategories).ThenInclude(x => x.Category)
-        //        .FirstOrDefaultAsync();
-        //    if (existProduct == null)
-        //        return NotFound();
-
-        //    var parentCategories = await _dbContext.Categories
-        //        .Where(x => x.IsDeleted == false && x.IsMain)
-        //        .Include(x => x.Children.Where(y => y.IsDeleted == false))
-        //        .ToListAsync();
-        //    var childCategories = parentCategories[0].Children.ToList();
-
-        //    productViewModel.ParentCategories = parentCategories;
-        //    productViewModel.ChildCategories = childCategories;
-        //    productViewModel.ProductImages = existProduct.ProductImages.ToList();
-
-        //    if (!ModelState.IsValid)
-        //        return View(productViewModel);
-
-        //    var isExistProduct = await _dbContext.Products
-        //        .AnyAsync(x => x.Name == productViewModel.Name && x.Id != productViewModel.Id);
-        //    if (isExistProduct)
-        //    {
-        //        ModelState.AddModelError("Name", "Bu adda mehsul movcuddur.");
-        //        return View(productViewModel);
-        //    }
-
-        //    var existParentCategory = parentCategories
-        //        .FirstOrDefault(x => x.IsDeleted == false &&
-        //                             x.IsMain &&
-        //                             x.Id == productViewModel.SelectedParentCategoryId);
-        //    if (existParentCategory == null)
-        //        return NotFound();
-
-        //    if (productViewModel.SelectedChildCategoryId != null)
-        //    {
-        //        if (!existParentCategory.Children.Any(x => x.Id == productViewModel.SelectedChildCategoryId))
-        //            return NotFound();
-
-        //        existProduct.ProductCategories
-        //            .FirstOrDefault(x => x.Category.IsDeleted == false && x.Category.IsMain == false)
-        //            .CategoryId = (int)productViewModel.SelectedChildCategoryId;
-        //    }
-
-        //    if (productViewModel.Photos != null && productViewModel.Photos.Length > 0)
-        //    {
-        //        foreach (var photo in productViewModel.Photos)
-        //        {
-        //            if (!photo.IsImage())
-        //            {
-        //                ModelState.AddModelError("Photos", $"{photo.Name} Duzgun shekil formati sechin.");
-        //                return View();
-        //            }
-
-        //            if (!photo.IsAllowedSize(1))
-        //            {
-        //                ModelState.AddModelError("Photos", $"{photo.Name} 1Mb-dan artiq ola bilmez.");
-        //                return View();
-        //            }
-
-        //            var fileName = await photo.GenerateFile(Constants.ImageFolderPath);
-
-        //            var productImage = new ProductImage
-        //            {
-        //                Name = fileName,
-        //                ProductId = (int)id
-        //            };
-
-        //            await _dbContext.ProductImages.AddAsync(productImage);
-        //            await _dbContext.SaveChangesAsync();
-        //        }
-        //    }
-
-        //    existProduct.Name = productViewModel.Name;
-        //    existProduct.Description = productViewModel.Description;
-        //    existProduct.Brand = productViewModel.Brand;
-        //    existProduct.Code = productViewModel.Code;
-        //    existProduct.ExTax = productViewModel.ExTax;
-        //    existProduct.Tags = productViewModel.Tags;
-        //    existProduct.Rate = productViewModel.Rate;
-        //    existProduct.Price = productViewModel.Price;
-        //    existProduct.DiscountPercent = productViewModel.DiscountPercent;
-
-        //    existProduct.ProductCategories
-        //        .FirstOrDefault(x => x.Category.IsDeleted == false && x.Category.IsMain)
-        //        .CategoryId = productViewModel.SelectedParentCategoryId;
-
-        //    await _dbContext.SaveChangesAsync();
-
-        //    return RedirectToAction("Index");
-        //}
 
     }
 }
